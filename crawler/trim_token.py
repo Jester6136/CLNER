a15b923b18925c7d0a17d1f3f32f1082a2ece0b7
Ba Son <bagiangson2001@gmail.com>
2023-04-12 12:37:19 +0700

    update

--- crawler/trim_token.py
+++ crawler/trim_token.py
@@ -9,10 +9,9 @@ if args.file_path:
     print(f'Trim data for {args.file_path}')
     print("===========================================")
 
-source_file_path = args.file_path
+source_file_path = r'datasets/MTL-Bioinformatics-2016-external-contexts/'+args.file_path
 
 count = 0
-max_tokens = 0
 for (_, _, filenames) in walk(source_file_path):
     for file_name in filenames:
         new_text = ''
@@ -22,14 +21,13 @@ for (_, _, filenames) in walk(source_file_path):
         tokens_block = []
         for item in sublists:
             tokens = item.split('\n')
-            if len(tokens) > max_tokens:
-                max_tokens = len(tokens)
-            if len(tokens) >249:
+            if len(tokens) >250:
                 count+=1
-                tokens = tokens[:249]
+                tokens = tokens[:250]
             new_text = '\n'.join(tokens)
             tokens_block.append(new_text)
         new_text = '\n\n'.join(tokens_block)
         print(count)
+        with open(os.path.join(source_file_path,file_name),'w',encoding='utf8') as f:
+            f.write(new_text)
 
-print(max_tokens)
